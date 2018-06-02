f9047f9ee20fb2da21f4a0d843aa3bcd44568643
Junior <35627119+Juniorsz@users.noreply.github.com>
2018-06-02 19:20:27 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+  error_reporting(0);
+  date_default_timezone_set('Asia/Ho_Chi_Minh');
+  $access_token = ""; // Nhập Token
+  $id = "100012668051362"; // ID người bạn muốn tăng share
+  $page_avoid = ['']; // ID của Page mà bạn không muốn share , các nhau bởi dấu "," VD : ["123","456"];
+
+  $page = json_decode(request("https://graph.facebook.com/me/accounts?access_token=$access_token"),TRUE);
+  $get_posts = json_decode(request("https://graph.facebook.com/$id/feed?limit=1&access_token=$access_token"),TRUE);
+  $firt_post_id = $get_posts['data'][0]['id'];
+  
+  foreach($page['data'] as $data){
+
+	  if(!in_array($data['id'],$page_avoid)){
+	    $result = request("https://graph.facebook.com/$firt_post_id/sharedposts?method=POST&access_token=".$data['access_token']."\n");
+		sleep(2);
+	  }
+  }
+  $response = json_decode($result,TRUE);
+  $time = date("y-m-d H:i:s");
+  $file = fopen('log.txt','a');
+  if($response['error']){
+	  echo "Bài viết phải ở chế độ công khai";
+	  fwrite($file,"$time : $firt_post_id - Lỗi !!! \n");
+  }
+  else{
+	  echo "Thành công";
+	  fwrite($file,"$time : $firt_post_id - Thành công !!! \n");
+  }
+  function request($url){
+	$ch = curl_init();
+	CURL_SETOPT_ARRAY($ch,array(
+		CURLOPT_URL => $url,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_RETURNTRANSFER => TRUE,
+		CURLOPT_SSL_VERIFYHOST => 2,
+		CURLOPT_SSL_VERIFYPEER => FALSE,
+		CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36"
+	));
+	$response = curl_exec($ch);
+	return $response;
+	curl_close($ch);
+}
+?>
+<meta http-equiv="refresh" content="30">
